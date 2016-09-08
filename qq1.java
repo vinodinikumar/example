052d68c9ef9e9271244eb476dde8b7dd5c7deeb1
vinodinikumar <vinodinikumar@users.noreply.github.com>
2016-09-08 16:49:01 +0530

    Create qq1.java

---
+++ qq1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class prime {
+    private static int[] z;
+    public static void main(String[] arg){
+        Scanner sc=new Scanner(System.in);
+       for(int j=0;j<5;j++)
+       {        int z=sc.nextInt();
+    }
+       for(int j=0;j<5;j++){
+           if(z[i]<100){
+              char []d= String.valueOf(z[i]).toCharArray();
+               System.out.println(d[i]+" "+d[i+1]);
+           }
+       }
+    
+}}
